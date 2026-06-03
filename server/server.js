b02e18214230364f8787824f31ff02ce53f73f5c
Garvit Goyal <garvitgoyal2k6@gmail.com>
2026-06-03 15:00:27 +0530

    fixing cors error

--- server/server.js
+++ server/server.js
@@ -1,38 +1,37 @@
-import express from 'express'
-import { config } from 'dotenv'
-import connectToDb from "./src/config/db.js"
-import cors from 'cors'
-import authRoutes from "./src/routes/auth.routes.js"
-import taskRoutes from "./src/routes/task.routes.js"
+import express from "express";
+import { config } from "dotenv";
+import connectToDb from "./src/config/db.js";
+import cors from "cors";
+import authRoutes from "./src/routes/auth.routes.js";
+import taskRoutes from "./src/routes/task.routes.js";
 
-config()
-const app= express()
+config();
+const app = express();
 
+const corsOptions = {
+  origin: ["http://localhost:5173", "https://task-manager-smoky-psi.vercel.app"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
-app.use(express.json())
-app.use(cors({
-    origin: ["http://localhost:5173", process.env.FRONTEND_URL],
-    methods: ["GET", "POST", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-}))
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
-//auth Routes
-app.use('/api/auth', authRoutes)
-app.use('/api/task',taskRoutes)
+app.use(express.json());
 
+app.use("/api/auth", authRoutes);
+app.use("/api/task", taskRoutes);
 
+const startServer = async () => {
+  try {
+    await connectToDb();
 
-const startServer= ()=>{
-    try {
-        connectToDb()
+    app.listen(process.env.PORT || 4500, () => {
+      console.log("server started on PORT", process.env.PORT || 4500);
+    });
+  } catch (error) {
+    console.log("error connecting to mongoDb", error);
+  }
+};
 
-        app.listen(process.env.PORT, ()=>{
-            console.log("server started on PORT", process.env.PORT)
-        })
-
-    } catch (error) {
-        console.log("error connecting to mongoDb")
-    }
-}
-
-startServer()
+startServer();
