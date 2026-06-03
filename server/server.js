395f6200a449c854717078e3d9542f88593adfc6
Garvit Goyal <garvitgoyal2k6@gmail.com>
2026-06-03 15:35:00 +0530

    fixing cors

--- server/server.js
+++ server/server.js
@@ -25,10 +25,11 @@ app.use(
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
       allowedHeaders: ["Content-Type", "Authorization"],
       credentials: true,
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
     })
   );
 
-  app.options("*", cors());
 
 app.use(express.json());
 
